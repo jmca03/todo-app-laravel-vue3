ea53f2fd48fb27aaed2faf942a19754f47118259
John Marvin Alarcon <johnmarvinalarcon@gmail.com>
2024-03-26 13:36:40 +0800

    FEATURE: Add Todo Language

---
+++ src/lang/en/todo.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+     * |--------------------------------------------------------------------------
+     * | Todo Language Lines
+     * |--------------------------------------------------------------------------
+     * |
+     * | The following language lines are used for Todo class
+     * |
+    */
+
+return [
+    'error_title' => 'Todo Error',
+    'error_subtitle' => 'Todo Error (:subtitle)',
+    'log' => [
+        'error_title' => 'Todo Log Error',
+        'error_subtitle' => 'Todo Log Error (:subtitle)',
+    ]
+];
