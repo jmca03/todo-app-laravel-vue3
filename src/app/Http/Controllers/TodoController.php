6e87983d16f42bc7849b3c581cbe9b925da9500f
John Marvin Alarcon <johnmarvinalarcon@gmail.com>
2024-03-26 13:34:16 +0800

    FEATURE: Construct the controller for the Todo

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,47 +2,76 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TodoRequest;
 use Illuminate\Http\Request;
+use App\Services\TodoService;
+use Illuminate\Http\JsonResponse;
 
 class TodoController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Constructor
+     * 
+     * @param TodoService $service
      */
-    public function index()
+    public function __construct(protected TodoService $service)
     {
-        //
+        $this->service = $service;
+    }
+
+    /**
+     * Display a listing of the resource.
+     * 
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(Request $request): JsonResponse
+    {
+        return $this->service->index($request);
     }
 
     /**
      * Store a newly created resource in storage.
+     * 
+     * @param \App\Http\Requests\TodoRequest $request
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function store(Request $request)
+    public function store(TodoRequest $request): JsonResponse
     {
-        //
+        return $this->service->store($request->toArray());
     }
 
     /**
      * Display the specified resource.
+     * 
+     * @param string|int $id
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function show(string $id)
+    public function show(string|int $id): JsonResponse
     {
-        //
+        return $this->service->show($id);
     }
 
     /**
      * Update the specified resource in storage.
+     * 
+     * @param \App\Http\Requests\TodoRequest $request
+     * @param string|int  $id
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function update(Request $request, string $id)
+    public function update(TodoRequest $request, string|int $id)
     {
-        //
+        return $this->service->update($id, $request->toArray());
     }
 
     /**
      * Remove the specified resource from storage.
+     * 
+     * @param string|int $id
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function destroy(string $id)
+    public function destroy(string $id): JsonResponse
     {
-        //
+        return $this->service->destroy($id);
     }
 }
