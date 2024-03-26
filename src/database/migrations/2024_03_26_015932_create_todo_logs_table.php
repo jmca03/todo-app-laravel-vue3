97c81e73fb63341c4b8725855732c0ee86b39994
John Marvin Alarcon <johnmarvinalarcon@gmail.com>
2024-03-26 10:32:18 +0800

    FEATURE: Add Todo Logs

---
+++ src/database/migrations/2024_03_26_015932_create_todo_logs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('todo_logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('todo_id')->constrained('todos')->nullOnDelete();
+            $table->text('content');
+            $table->text('remarks');
+            $table->timestamp('scheduled_at');
+            $table->timestamp('expired_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('todo_logs');
+    }
+};
