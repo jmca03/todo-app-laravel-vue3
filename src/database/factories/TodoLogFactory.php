bb7c5cbc99cfce6389879d8ab8c47b0452638788
John Marvin Alarcon <johnmarvinalarcon@gmail.com>
2024-03-26 12:47:33 +0800

    FEATURE: Create Todo and Todo Log Factory class

---
+++ src/database/factories/TodoLogFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Todo;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\TodoLog>
+ */
+class TodoLogFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'todo_id' => Todo::factory(),
+            'content' => fake()->text(),
+            'scheduled_at' => now(),
+            'expired_at' => now()
+        ];
+    }
+}
