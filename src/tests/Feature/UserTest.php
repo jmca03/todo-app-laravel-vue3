8f7043711a0ccf0726cb4f0b3e98296f38f2da36
John Marvin Alarcon <johnmarvinalarcon@gmail.com>
2024-03-26 20:07:54 +0800

    TEST: User initial test case

---
+++ src/tests/Feature/UserTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use DatabaseTransactions, WithFaker;
+    
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
