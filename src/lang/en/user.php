b005bef6a469d436c5a35ec818402c80c0859496
John Marvin Alarcon <johnmarvinalarcon@gmail.com>
2024-03-26 20:07:29 +0800

    FEATURE: Create Language for user

---
+++ src/lang/en/user.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+     * |--------------------------------------------------------------------------
+     * | User Language Lines
+     * |--------------------------------------------------------------------------
+     * |
+     * | The following language lines are used for Todo class
+     * |
+    */
+
+return [
+    'error_title' => 'User Error',
+    'error_subtitle' => 'User Error (:subtitle)',
+    'log' => [
+        'error_title' => 'User Log Error',
+        'error_subtitle' => 'User Log Error (:subtitle)',
+    ]
+];
