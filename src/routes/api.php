b35e12f2609fec65698c0a2af4e414eb3e8d7877
John Marvin Alarcon <johnmarvinalarcon@gmail.com>
2024-03-27 17:26:39 +0800

    FEATURE: Addition of user registration on route

--- src/routes/api.php
+++ src/routes/api.php
@@ -45,6 +45,7 @@ Route::get('health-check', fn () => Response::json([
  */
 Route::prefix('auth')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
 
     Route::middleware('auth:api')->group(function () {
         Route::post('logout', [AuthController::class, 'logout']);
