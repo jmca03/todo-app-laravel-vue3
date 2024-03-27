dba5555cc0de2b57fb4295efe6a7b4c68af328ad
John Marvin Alarcon <johnmarvinalarcon@gmail.com>
2024-03-27 12:25:06 +0800

    FEATURE: Add Passport methods on Auth service provider

--- src/app/Providers/AuthServiceProvider.php
+++ src/app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use Laravel\Passport\Passport;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,6 +22,9 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Passport::loadKeysFrom(storage_path());
+        Passport::tokensExpireIn(now()->add(unit: config('token.expires_in_unit'), value: config('token.expires_in')));
+        Passport::refreshTokensExpireIn(now()->add(unit: config('token.refresh.expires_in_unit'), value: config('token.refresh.expires_in')));
+        Passport::personalAccessTokensExpireIn(now()->add(unit: config('token.personal.expires_in_unit'), value: config('token.personal.expires_in')));
     }
 }
