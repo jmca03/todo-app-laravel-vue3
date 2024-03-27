dbda9d407b6e385c999b81f3e0b2a14e7b1f07b8
John Marvin Alarcon <johnmarvinalarcon@gmail.com>
2024-03-27 12:28:46 +0800

    TEST: User test CRUD test cases all passed

--- src/tests/Feature/UserTest.php
+++ src/tests/Feature/UserTest.php
@@ -2,24 +2,127 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class UserTest extends TestCase
 {
     use DatabaseTransactions, WithFaker;
-    
+
+    /** @var string */
+    protected string $route = '/api/user';
+
     /**
-     * A basic feature test example.
-     *
+     * Test index
+     * 
      * @return void
      */
-    public function test_example(): void
+    public function test_index(): void
     {
-        $response = $this->get('/');
+        // Arrange
+        $user = User::factory()->create();
 
-        $response->assertStatus(200);
+        // Act
+        $response = $this->actingAs($user, 'api')->getJson($this->route);
+
+        // Assert
+        $response->assertOk();
+    }
+
+    /**
+     * Test show
+     * 
+     * @return void
+     */
+    public function test_show(): void
+    {
+        // Arrange
+        $user = User::factory()->create();
+        /** @var string|int */
+        $id = $user->id;
+
+        // Act
+        $response = $this->actingAs($user, 'api')->getJson($this->route . '/' . $id);
+
+        // Assert
+        $response->assertOk();
+    }
+
+    /**
+     * Test create
+     * 
+     * @return void
+     */
+    public function test_create(): void
+    {
+        // Arrange
+        $user = User::factory()->create();
+
+        /**
+         * @var array<object>
+         */
+        $payload = [
+            'name' => $this->faker->name(),
+            'username' => $this->faker->unique()->userName(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => 'Password@123',
+        ];
+
+        // Act
+        $response = $this->actingAs($user, 'api')->postJson($this->route . '/', $payload);
+
+        // Assert
+        $response->assertCreated();
+    }
+
+    /**
+     * Test update
+     * 
+     * @return void
+     */
+    public function test_update(): void
+    {
+        // Arrange
+        $user = User::factory()->create();
+        /** @var string|int */
+        $id = $user->id;
+
+        /**
+         * @var array<object>
+         */
+        $payload = [
+            'name' => $this->faker->name(),
+            'username' => $this->faker->unique()->userName(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => 'Password@123',
+        ];
+
+        // Act
+        $response = $this->actingAs($user, 'api')->patchJson($this->route . '/' . $id, $payload);
+
+        // Assert
+        $response->assertOk();
+    }
+
+    /**
+     * Test delete
+     * 
+     * @return void
+     */
+    public function test_delete(): void
+    {
+        // Arrange
+        $user = User::factory()->create();
+        /** @var string|int */
+        $id = $user->id;
+
+        // Act
+        $response = $this->actingAs($user, 'api')->deleteJson($this->route . '/' . $id);
+
+        // Assert
+        $response->assertOk();
     }
 }
