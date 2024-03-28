288d9cfdc29656910a1b2e0c90e40ce8e4323408
John Marvin Alarcon <johnmarvinalarcon@gmail.com>
2024-03-28 10:52:03 +0800

    FEATURE: Add sequence on todos migration

--- src/database/migrations/2024_03_17_004103_create_todos_table.php
+++ src/database/migrations/2024_03_17_004103_create_todos_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->nullable()->constrained(table: 'users')->nullOnDelete();
+            $table->bigInteger('sequence');
             $table->text('content');
             $table->timestamp('scheduled_at')->nullable();
             $table->timestamp('expired_at')->nullable();
